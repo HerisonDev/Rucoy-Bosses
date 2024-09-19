5555c459195d9783942b5159cecc3f037a2cc50e
Herison <rucoyconta72@gmail.com>
2024-09-19 14:46:08 +0000

    update rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
     {
+        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 1
+    },
+    {
         nick: "Yaguchi ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20ha", pontos: 1
     },
 

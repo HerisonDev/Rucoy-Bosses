849f53fba2a6e50eedc71628bdbb66861b27de42
Herison <rucoyconta72@gmail.com>
2024-09-19 15:25:30 +0000

    update rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
     {
+        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1
+    },
+    {
         nick: "Little Sunshine", charLink: "https://www.rucoyonline.com/characters/Little%20Sunshine", pontos: 1
     },
     {

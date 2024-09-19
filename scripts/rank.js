0c9a3f194fc0686560b40d025489fd04f35136c8
Herison <herisonguimaraes@gmail.com>
2024-09-18 22:10:24 -0300

    update rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
   {
+    nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 1
+  },
+  {
     nick: "Teste", charLink: "https://www.rucoyonline.com/characters/Teste", pontos: 1
   },
     

66d9b82a0664f3f2e3086d3008b7013adfeda0d1
Herison <rucoyconta72@gmail.com>
2024-09-19 02:25:19 +0000

    update rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
     {
+        nick: "Blanda", charLink: "https://www.rucoyonline.com/characters/Blanda", pontos: 1
+    },
+    {
         nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 2
     },
 

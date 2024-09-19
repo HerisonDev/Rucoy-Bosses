4536a3a4083706a43284d7c4bc16b9a6dbe76988
Herison <herisonguimaraes@gmail.com>
2024-09-18 22:25:39 -0300

    update rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
   {
+    nick: "Ursula", charLink: "https://www.rucoyonline.com/characters/Ursula", pontos: 1
+  },
+  {
     nick: "Isa belinha", charLink: "https://www.rucoyonline.com/characters/Isa%20belinha", pontos: 2
   },
   {

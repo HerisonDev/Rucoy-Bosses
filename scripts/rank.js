b6f9b30848234f3d6a4f3c43d0501e5ae2663801
HerisonDev <rucoyconta72@gmail.com>
2024-09-18 22:38:20 -0300

    Update rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,19 +1,4 @@
 let jogadores = [
-  {
-    nick: "Ursula", charLink: "https://www.rucoyonline.com/characters/Ursula", pontos: 1
-  },
-  {
-    nick: "Isa belinha", charLink: "https://www.rucoyonline.com/characters/Isa%20belinha", pontos: 2
-  },
-  {
-    nick: "Joseph", charLink: "https://www.rucoyonline.com/characters/Joseph", pontos: 1
-  },
-  {
-    nick: "Bestia", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 2
-  },
-  {
-    nick: "Teste", charLink: "https://www.rucoyonline.com/characters/Teste", pontos: 1
-  },
     
 ];
 

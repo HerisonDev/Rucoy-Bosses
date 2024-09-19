bc7a46b5cddb20f8c9dfdd2bed3ed34cf0fc0bbd
Herison <rucoyconta72@gmail.com>
2024-09-19 14:49:17 +0000

    erase rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,12 +1,3 @@
 let jogadores = [
-    {
-        nick: "Herobrine", charLink: "https://www.rucoyonline.com/characters/Herobrine", pontos: 1
-    },
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 1
-    },
-    {
-        nick: "Yaguchi ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20ha", pontos: 1
-    },
 
 ];

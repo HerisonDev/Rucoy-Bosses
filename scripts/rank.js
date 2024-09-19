6e9391f7ceac2f97f6aeca9609bb93982a62b27a
Herison <rucoyconta72@gmail.com>
2024-09-19 10:25:05 +0000

    erase rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,18 +1,3 @@
 let jogadores = [
-    {
-        nick: "Nfalo so farmo", charLink: "https://www.rucoyonline.com/characters/Nfalo%20so%20farmo", pontos: 1
-    },
-    {
-        nick: "Ana Beatriz", charLink: "https://www.rucoyonline.com/characters/Ana%20Beatriz", pontos: 2
-    },
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 2
-    },
-    {
-        nick: "Blanda", charLink: "https://www.rucoyonline.com/characters/Blanda", pontos: 1
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 2
-    },
 
 ];

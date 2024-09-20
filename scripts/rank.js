d1721ff009aae02e7578f9dbf6eb614ec508bfb7
Herison <rucoyconta72@gmail.com>
2024-09-20 00:06:21 +0000

    erase history.js and rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,39 +1,3 @@
 let jogadores = [
-    {
-        nick: "Zeze", charLink: "https://www.rucoyonline.com/characters/Zeze", pontos: 1
-    },
-    {
-        nick: "Zuniga", charLink: "https://www.rucoyonline.com/characters/Zuniga", pontos: 1
-    },
-    {
-        nick: "Soubr", charLink: "https://www.rucoyonline.com/characters/Soubr", pontos: 1
-    },
-    {
-        nick: "Gusta vendas", charLink: "https://www.rucoyonline.com/characters/Gusta%20vendas", pontos: 1
-    },
-    {
-        nick: "Manoel gome", charLink: "https://www.rucoyonline.com/characters/Manoel%20gome", pontos: 1
-    },
-    {
-        nick: "Gm Rucoy", charLink: "https://www.rucoyonline.com/characters/Gm%20Rucoy", pontos: 2
-    },
-    {
-        nick: "Manar", charLink: "https://www.rucoyonline.com/characters/Manar", pontos: 1
-    },
-    {
-        nick: "Nfalo so farmo", charLink: "https://www.rucoyonline.com/characters/Nfalo%20so%20farmo", pontos: 2
-    },
-    {
-        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", pontos: 1
-    },
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 3
-    },
-    {
-        nick: "Josias", charLink: "https://www.rucoyonline.com/characters/Josias", pontos: 2
-    },
-    {
-        nick: "Yhwach", charLink: "https://www.rucoyonline.com/characters/Yhwach", pontos: 2
-    },
 
 ];

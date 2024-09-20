47f49227a69b546e6801d7c49f089d5e5ea74655
Herison <rucoyconta72@gmail.com>
2024-09-20 23:20:59 +0000

    fix typo

--- scripts/app.js
+++ scripts/app.js
@@ -76,7 +76,7 @@ limitedFound = found.slice(0, 5);
 for(let history of limitedFound){
   historyContent +=
   `
-  <p><span class="time-span">${history.date} :</span> <a href="${history.charLink target="_blank"}">${history.nick}</a> found <span class="boss-name">Drakon The Devourer</span> on ${history.server}.</p>
+  <p><span class="time-span">${history.date} :</span> <a href="${history.charLink}" target="_blank">${history.nick}</a> found <span class="boss-name">Drakon The Devourer</span> on ${history.server}.</p>
   `
 }
 

ad1283204d9ddfab6b7142ce762a65c98cb10f6d
Herison <rucoyconta72@gmail.com>
2024-09-20 00:50:17 +0000

    erase history.js and rank.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,15 +1,3 @@
 let found = [
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "Asia 2", date: "20 Sep 00:27:22"
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "South America 2", date: "20 Sep 00:25:41"
-    },
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "America 3", date: "20 Sep 00:24:11"
-    },
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "South America 2", date: "20 Sep 00:22:49"
-    },
-
+    
 ];

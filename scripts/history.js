52f82e59bf6ec64e3565a9ce2a7a9829494ff2dd
Herison <rucoyconta72@gmail.com>
2024-09-20 17:56:18 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,3 +1,6 @@
 let found = [
+    {
+        nick: "", charLink: "https://www.rucoyonline.com/characters/", server: "", date: "20 Sep 17:56:18"
+    },
     
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,3 +1,6 @@
 let jogadores = [
+    {
+        nick: "", charLink: "https://www.rucoyonline.com/characters/", pontos: 1
+    },
 
 ];

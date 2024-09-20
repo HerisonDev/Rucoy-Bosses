8704095c42e0419a621def75ed8dd7510ca407d1
Herison <rucoyconta72@gmail.com>
2024-09-20 21:13:40 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Ehre", charLink: "https://www.rucoyonline.com/characters/Ehre", server: "Asia 3", date: "20 Sep 21:13:40"
+    },
+    {
         nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "Asia 3", date: "20 Sep 21:12:22"
     },
     {
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
     {
+        nick: "Ehre", charLink: "https://www.rucoyonline.com/characters/Ehre", pontos: 1
+    },
+    {
         nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 1
     },
     {

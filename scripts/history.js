e56da5a7491af8d41f92eada46e791484518d4d0
Herison <rucoyconta72@gmail.com>
2024-09-20 00:24:11 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "America 3", date: "20 Sep 00:24:11"
+    },
+    {
         nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "South America 2", date: "20 Sep 00:22:49"
     },
 
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,6 +1,6 @@
 let jogadores = [
     {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1
+        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 2
     },
 
 ];

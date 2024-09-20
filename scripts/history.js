c35faff3fd24913bec9fc6ff0be04c9bd4420596
Herison <rucoyconta72@gmail.com>
2024-09-20 00:22:49 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,3 +1,6 @@
 let found = [
+    {
+        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "South America 2", date: "20 Sep 00:22:49"
+    },
 
 ];

f2e6570d704da429bf1c3c28d9b0efc5a783e038
Herison <rucoyconta72@gmail.com>
2024-09-20 21:09:43 +0000

    erase history and rank

--- scripts/history.js
+++ scripts/history.js
@@ -1,9 +1,3 @@
 let found = [
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "America2", date: "20 Sep 21:04:56"
-    },
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "South America 5", date: "20 Sep 20:59:12"
-    },
 
 ];

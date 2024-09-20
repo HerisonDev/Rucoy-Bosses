533fa98de2de3b25a4c044f07d30181ba111cf3a
Herison <rucoyconta72@gmail.com>
2024-09-20 21:11:21 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,3 +1,6 @@
 let found = [
+    {
+        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "North America 2", date: "20 Sep 21:11:21"
+    },
 
 ];

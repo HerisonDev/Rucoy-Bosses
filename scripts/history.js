78a9a671c7f7b017d92bffa78cd72abec79c57c4
Herison <rucoyconta72@gmail.com>
2024-09-21 02:14:23 +0000

    erase history.js and rank.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,21 +1,3 @@
 let found = [
-    {
-        nick: "Isaias", charLink: "https://www.rucoyonline.com/characters/Isaias", server: "Europe 2", date: "20 Sep 23:07:45"
-    },
-    {
-        nick: "Nfalo So Farmo", charLink: "https://www.rucoyonline.com/characters/Nfalo%20So%20Farmo", server: "South America 1", date: "20 Sep 21:17:54"
-    },
-    {
-        nick: "Ehre", charLink: "https://www.rucoyonline.com/characters/Ehre", server: "Europe 1", date: "20 Sep 21:15:16"
-    },
-    {
-        nick: "Ehre", charLink: "https://www.rucoyonline.com/characters/Ehre", server: "Asia 3", date: "20 Sep 21:13:40"
-    },
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "Asia 3", date: "20 Sep 21:12:22"
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "North America 2", date: "20 Sep 21:11:21"
-    },
 
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,18 +1,3 @@
 let jogadores = [
-    {
-        nick: "Isaias", charLink: "https://www.rucoyonline.com/characters/Isaias", pontos: 1
-    },
-    {
-        nick: "Nfalo So Farmo", charLink: "https://www.rucoyonline.com/characters/Nfalo%20So%20Farmo", pontos: 1
-    },
-    {
-        nick: "Ehre", charLink: "https://www.rucoyonline.com/characters/Ehre", pontos: 2
-    },
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 1
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 1
-    },
 
 ];

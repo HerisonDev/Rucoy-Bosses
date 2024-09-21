3802be8002ad6bd1db7d826c71d57198799cb4d7
Herison <rucoyconta72@gmail.com>
2024-09-21 20:40:35 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Josias", charLink: "https://www.rucoyonline.com/characters/Josias", server: "North America 2", date: "21 Sep 20:40:35"
+    },
+    {
         nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "North America 1", date: "21 Sep 20:38:05"
     },
 
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
     {
+        nick: "Josias", charLink: "https://www.rucoyonline.com/characters/Josias", pontos: 1
+    },
+    {
         nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1
     },
 

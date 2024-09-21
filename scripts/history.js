f4f3e8c6d8815c98f56699cbd351f97e76248d7c
Herison <rucoyconta72@gmail.com>
2024-09-21 12:29:15 +0000

    erase rank and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,24 +1,3 @@
 let found = [
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "Europe 5", date: "21 Sep 12:23:42"
-    },
-    {
-        nick: "er", charLink: "https://www.rucoyonline.com/characters/er", server: "South America 8", date: "21 Sep 03:39:13"
-    },
-    {
-        nick: "Vu", charLink: "https://www.rucoyonline.com/characters/Vu", server: "Asia 3", date: "21 Sep 03:37:51"
-    },
-    {
-        nick: "Yhwach", charLink: "https://www.rucoyonline.com/characters/Yhwach", server: "Asia 4", date: "21 Sep 03:36:30"
-    },
-    {
-        nick: "Fallen Kanroji", charLink: "https://www.rucoyonline.com/characters/Fallen%20Kanroji", server: "North America 1", date: "21 Sep 02:26:18"
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "North America 1", date: "21 Sep 02:23:31"
-    },
-    {
-        nick: "Fallen Kanjori", charLink: "https://www.rucoyonline.com/characters/Fallen%20Kanjori", server: "South America 4", date: "21 Sep 02:15:52"
-    },
 
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,6 +1,3 @@
 let jogadores = [
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1
-    },
 
 ];

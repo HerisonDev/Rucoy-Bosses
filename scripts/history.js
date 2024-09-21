5c8d352abe722840f2bdedae64f7e7e326112a14
Herison <rucoyconta72@gmail.com>
2024-09-21 02:23:31 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "North America 1", date: "21 Sep 02:23:31"
+    },
+    {
         nick: "Fallen Kanjori", charLink: "https://www.rucoyonline.com/characters/Fallen%20Kanjori", server: "South America 4", date: "21 Sep 02:15:52"
     },
 
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
     {
+        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 1
+    },
+    {
         nick: "Fallen Kanjori", charLink: "https://www.rucoyonline.com/characters/Fallen%20Kanjori", pontos: 1
     },
 

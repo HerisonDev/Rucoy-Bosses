94f80a40f4d4009aaaba84a92b11a9ec48725ab8
Herison <rucoyconta72@gmail.com>
2024-09-21 19:10:33 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "North America 1", date: "21 Sep 19:10:33"
+    },
+    {
         nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "North America 1", date: "21 Sep 18:34:50"
     },
     {

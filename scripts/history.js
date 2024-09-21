6f8e0dc85dfd7c780121964c531bfec7b1f75da2
Herison <rucoyconta72@gmail.com>
2024-09-21 02:15:52 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,3 +1,6 @@
 let found = [
+    {
+        nick: "Fallen Kanjori", charLink: "https://www.rucoyonline.com/characters/Fallen%20Kanjori", server: "South America 4", date: "21 Sep 02:15:52"
+    },
 
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,3 +1,6 @@
 let jogadores = [
+    {
+        nick: "Fallen Kanjori", charLink: "https://www.rucoyonline.com/characters/Fallen%20Kanjori", pontos: 1
+    },
 
 ];

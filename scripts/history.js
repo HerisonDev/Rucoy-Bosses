56ea6246f3592c66c3448ffe0422f300fe205127
Herison <rucoyconta72@gmail.com>
2024-09-21 13:12:22 +0000

    erasing

--- scripts/history.js
+++ scripts/history.js
@@ -1,9 +1,3 @@
 let found = [
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "Europe 1", date: "21 Sep 13:10:19"
-    },
-    {
-        nick: "Yudi", charLink: "https://www.rucoyonline.com/characters/Yudi", server: "North America 1", date: "21 Sep 12:45:52"
-    },
 
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,9 +1,3 @@
 let jogadores = [
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 1
-    },
-    {
-        nick: "Yudi", charLink: "https://www.rucoyonline.com/characters/Yudi", pontos: 1
-    },
 
 ];

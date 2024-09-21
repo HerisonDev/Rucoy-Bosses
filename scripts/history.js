41de1cc39c4e7c3e0c0983e78b5107ed98269d02
Herison <rucoyconta72@gmail.com>
2024-09-21 13:10:19 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "Europe 1", date: "21 Sep 13:10:19"
+    },
+    {
         nick: "Yudi", charLink: "https://www.rucoyonline.com/characters/Yudi", server: "North America 1", date: "21 Sep 12:45:52"
     },
 
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
     {
+        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 1
+    },
+    {
         nick: "Yudi", charLink: "https://www.rucoyonline.com/characters/Yudi", pontos: 1
     },
 

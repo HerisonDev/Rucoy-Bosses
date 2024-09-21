39259eff640d047af5f66ca9bc68d9b7f0399ea8
Herison <rucoyconta72@gmail.com>
2024-09-21 19:58:19 +0000

    testing

--- scripts/history.js
+++ scripts/history.js
@@ -1,24 +1,3 @@
 let found = [
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "North America 1", date: "21 Sep 19:56:50"
-    },
-    {
-        nick: "Josias", charLink: "https://www.rucoyonline.com/characters/Josias", server: "North America 2", date: "21 Sep 19:48:11"
-    },
-    {
-        nick: "Qwe", charLink: "https://www.rucoyonline.com/characters/Qwe", server: "North America 1", date: "21 Sep 19:47:41"
-    },
-    {
-        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", server: "North America 1", date: "21 Sep 19:42:12"
-    },
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "North America 1", date: "21 Sep 19:10:33"
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "North America 1", date: "21 Sep 18:34:50"
-    },
-    {
-        nick: "yurii", charLink: "https://www.rucoyonline.com/characters/yurii", server: "North America 1", date: "21 Sep 13:51:39"
-    },
 
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,24 +1,3 @@
 let jogadores = [
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1
-    },
-    {
-        nick: "Josias", charLink: "https://www.rucoyonline.com/characters/Josias", pontos: 1
-    },
-    {
-        nick: "Qwe", charLink: "https://www.rucoyonline.com/characters/Qwe", pontos: 1
-    },
-    {
-        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", pontos: 1
-    },
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 1
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 1
-    },
-    {
-        nick: "yurii", charLink: "https://www.rucoyonline.com/characters/yurii", pontos: 1
-    },
 
 ];

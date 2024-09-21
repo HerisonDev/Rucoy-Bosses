be80e6b1346ed7bd542426e81f7debc73015a5cf
Herison <rucoyconta72@gmail.com>
2024-09-21 12:45:52 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,3 +1,6 @@
 let found = [
+    {
+        nick: "Yudi", charLink: "https://www.rucoyonline.com/characters/Yudi", server: "North America 1", date: "21 Sep 12:45:52"
+    },
 
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,3 +1,6 @@
 let jogadores = [
+    {
+        nick: "Yudi", charLink: "https://www.rucoyonline.com/characters/Yudi", pontos: 1
+    },
 
 ];

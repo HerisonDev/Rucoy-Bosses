61be9143827eb428661920d7bf5d6e2a53337077
Herison <rucoyconta72@gmail.com>
2024-09-21 12:19:50 +0000

    erase rank.js and history.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,21 +1,3 @@
 let jogadores = [
-    {
-        nick: "er", charLink: "https://www.rucoyonline.com/characters/er", pontos: 1
-    },
-    {
-        nick: "Vu", charLink: "https://www.rucoyonline.com/characters/Vu", pontos: 1
-    },
-    {
-        nick: "Yhwach", charLink: "https://www.rucoyonline.com/characters/Yhwach", pontos: 1
-    },
-    {
-        nick: "Fallen Kanroji", charLink: "https://www.rucoyonline.com/characters/Fallen%20Kanroji", pontos: 1
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 1
-    },
-    {
-        nick: "Fallen Kanjori", charLink: "https://www.rucoyonline.com/characters/Fallen%20Kanjori", pontos: 1
-    },
 
 ];

79f796a866413f1fd9e5688e1ca7dc5b91dc8e5e
Herison <rucoyconta72@gmail.com>
2024-10-05 20:58:06 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 4", date: "05 Oct 20:58:06"
+    },
+    {
         nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 1", date: "05 Oct 20:57:30"
     },
     

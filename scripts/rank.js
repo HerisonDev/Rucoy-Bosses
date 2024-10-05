3d9070ebe4ccd17b744314d516da0629a5347a60
HerisonDev <rucoyconta72@gmail.com>
2024-10-05 20:27:26 +0000

    Update rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,9 +1,3 @@
 let jogadores = [
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 2
-    },
     
-    { nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 2 },
-    { nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 2 },
-    { nick: "Maria mariah", charLink: "https://www.rucoyonline.com/characters/Maria%20mariah", pontos: 1 },
 ]

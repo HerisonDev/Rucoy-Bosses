9874fd47fc5d2abbdff7b997a1daa14ca2737638
Herison <rucoyconta72@gmail.com>
2024-10-05 18:21:38 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 3", date: "05 Oct 18:21:38"
+    },
+    {
         nick: "Isabele trans", charLink: "https://www.rucoyonline.com/characters/Isabele%20trans", server: "Asia 4", date: "05 Oct 18:15:07"
     },
     {
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,7 +1,7 @@
 let jogadores = [
     {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 1
-    }
+        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 2
+    },
     
     { nick: "Isabele trans", charLink: "https://www.rucoyonline.com/characters/Isabele%20trans", pontos: 1 },
 ]

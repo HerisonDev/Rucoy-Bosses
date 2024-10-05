0a4a2072c5fa24d89754d1735e2790b9a1b403fc
Herison <rucoyconta72@gmail.com>
2024-10-05 20:44:00 +0000

    update rank.js and history.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -3,4 +3,5 @@ let jogadores = [
         nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 1
     }
     
+    { nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1 },
 ]

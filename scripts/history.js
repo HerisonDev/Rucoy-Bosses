c54b6b4592cfbdb99902cf5cb1ab20652a0f97ac
Herison <rucoyconta72@gmail.com>
2024-10-05 20:51:56 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,3 +1,6 @@
 let found = [
+    {
+        nick: "Bestial Cracudo", charLink: "https://www.rucoyonline.com/characters/Bestial%20Cracudo", server: "Asia 4", date: "05 Oct 20:51:56"
+    },
         
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,3 +1,6 @@
 let jogadores = [
+    {
+        nick: "Bestial Cracudo", charLink: "https://www.rucoyonline.com/characters/Bestial%20Cracudo", pontos: 1
+    }
     
 ]

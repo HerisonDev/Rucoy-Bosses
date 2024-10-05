a4f06ffe0c83e098de40730d78437843dc2438c5
Herison <rucoyconta72@gmail.com>
2024-10-05 20:56:33 +0000

    update

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,7 +1,3 @@
 let jogadores = [
-    {
-        nick: "Bestial Cracudo", charLink: "https://www.rucoyonline.com/characters/Bestial%20Cracudo", pontos: 1
-    }
     
-    ,{        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 1\n        },
 ]

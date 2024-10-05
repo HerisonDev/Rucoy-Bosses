99656d44d781d1319cb13580c4b3979ebae7e2c6
HerisonDev <rucoyconta72@gmail.com>
2024-10-05 10:24:10 -0300

    Update history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,18 +1,3 @@
 let found = [
-    {
-        nick: "2", charLink: "https://www.rucoyonline.com/characters/2", server: "Europe 2", date: "21 Sep 21:20:23"
-    },
-    {
-        nick: "Teste", charLink: "https://www.rucoyonline.com/characters/Teste", server: "South America 3", date: "21 Sep 21:19:09"
-    },
-    {
-        nick: "Teste", charLink: "https://www.rucoyonline.com/characters/Teste", server: "Europe 5", date: "21 Sep 21:03:57"
-    },
-    {
-        nick: "Josias", charLink: "https://www.rucoyonline.com/characters/Josias", server: "North America 2", date: "21 Sep 20:40:35"
-    },
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "North America 1", date: "21 Sep 20:38:05"
-    },
 
 ];

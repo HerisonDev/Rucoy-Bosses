72845ac9f21d97d5c16358623366f25b9c00c21c
Herison <rucoyconta72@gmail.com>
2024-10-05 20:53:11 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "Europe 1", date: "05 Oct 20:53:11"
+    },
+    {
         nick: "Bestial Cracudo", charLink: "https://www.rucoyonline.com/characters/Bestial%20Cracudo", server: "Asia 4", date: "05 Oct 20:51:56"
     },
         

e77ec70c24eb10ae711a9a896f29e1bfe49c8a8e
HerisonDev <rucoyconta72@gmail.com>
2024-10-05 20:27:52 +0000

    Update history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,24 +1,3 @@
 let found = [
-    {
-        nick: "Maria mariah", charLink: "https://www.rucoyonline.com/characters/Maria%20mariah", server: "South America 5", date: "05 Oct 20:23:35"
-    },
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "South America 8", date: "05 Oct 20:22:50"
-    },
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "Asia 1", date: "05 Oct 18:38:28"
-    },
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "Asia 4", date: "05 Oct 18:38:01"
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "South America 1", date: "05 Oct 18:35:58"
-    },
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "Europe 6", date: "05 Oct 18:32:48"
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "Asia 1", date: "05 Oct 18:30:51"
-    },
-        
+    
 ];

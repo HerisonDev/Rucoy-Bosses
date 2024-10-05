44cf7b2ced6b77540ca486bb3d42272f0a6d06f6
Herison <rucoyconta72@gmail.com>
2024-10-05 20:59:36 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Isabela Gostosinha", charLink: "https://www.rucoyonline.com/characters/Isabela%20Gostosinha", server: "Europe 1", date: "05 Oct 20:59:36"
+    },
+    {
         nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 4", date: "05 Oct 20:58:06"
     },
     {
--- scripts/rank.js
+++ scripts/rank.js
@@ -3,4 +3,5 @@ let jogadores = [
         nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 2
     }
     
+    { nick: "Isabela Gostosinha", charLink: "https://www.rucoyonline.com/characters/Isabela%20Gostosinha", pontos: 1 },
 ]

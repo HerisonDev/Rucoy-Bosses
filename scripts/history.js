dde256403d378d5536b66fc3b778c0bae7bdd263
Herison <herisonguimaraes@gmail.com>
2024-10-05 13:33:36 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Graciosa", charLink: "https://www.rucoyonline.com/characters/Graciosa", server: "Asia 1", date: "05 Oct 13:33:36"
+    },
+    {
         nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Europe 1", date: "05 Oct 13:31:26"
     },
 
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,8 @@
 let jogadores = [
     {
+        nick: "Graciosa", charLink: "https://www.rucoyonline.com/characters/Graciosa", pontos: 1
+    },
+    {
         nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 1
     },
     

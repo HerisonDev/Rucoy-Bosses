0a9e6f92b9eb38ecd71c85d0affb74f80ef06fc4
Herison <rucoyconta72@gmail.com>
2024-10-05 20:54:10 +0000

    update index.html boss status

--- scripts/rank.js
+++ scripts/rank.js
@@ -3,5 +3,5 @@ let jogadores = [
         nick: "Bestial Cracudo", charLink: "https://www.rucoyonline.com/characters/Bestial%20Cracudo", pontos: 1
     }
     
-    ,{\n        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 1\n        },
+    ,{        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 1\n        },
 ]

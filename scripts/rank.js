bf3133990608db0b176f9cc1d4acef83b3b55fa8
HerisonDev <rucoyconta72@gmail.com>
2024-10-05 13:36:47 +0000

    Update rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,9 +1,3 @@
 let jogadores = [
-    {
-        nick: "Graciosa", charLink: "https://www.rucoyonline.com/characters/Graciosa", pontos: 2
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 1
-    },
     
 ];

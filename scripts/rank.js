2dd6f1664f838c572ab2d88e66053f0cb0fcf3b4
HerisonDev <rucoyconta72@gmail.com>
2024-10-05 10:24:52 -0300

    Update rank.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,15 +1,3 @@
 let jogadores = [
-    {
-        nick: "2", charLink: "https://www.rucoyonline.com/characters/2", pontos: 1
-    },
-    {
-        nick: "Teste", charLink: "https://www.rucoyonline.com/characters/Teste", pontos: 2
-    },
-    {
-        nick: "Josias", charLink: "https://www.rucoyonline.com/characters/Josias", pontos: 1
-    },
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1
-    },
-
+    
 ];

ce2d6a16998b9e535357fc860fc2d173c7674398
Herison <rucoyconta72@gmail.com>
2024-10-05 21:06:22 +0000

    Erasing

--- scripts/history.js
+++ scripts/history.js
@@ -1,27 +1,3 @@
 let found = [
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "South America 8", date: "05 Oct 21:05:21"
-    },
-    {
-        nick: "Beatriz", charLink: "https://www.rucoyonline.com/characters/Beatriz", server: "Europe 3", date: "05 Oct 21:04:59"
-    },
-    {
-        nick: "Beatriz", charLink: "https://www.rucoyonline.com/characters/Beatriz", server: "Europe 5", date: "05 Oct 21:04:28"
-    },
-    {
-        nick: "Beatriz", charLink: "https://www.rucoyonline.com/characters/Beatriz", server: "Europe 6", date: "05 Oct 21:04:17"
-    },
-    {
-        nick: "Beatriz", charLink: "https://www.rucoyonline.com/characters/Beatriz", server: "Asia 2", date: "05 Oct 21:03:42"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 1", date: "05 Oct 21:03:09"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 3", date: "05 Oct 21:02:27"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 4", date: "05 Oct 21:02:02"
-    },
-        
+            
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,7 +1,3 @@
 let jogadores = [
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 4
-    }
     
-    ,{ nick: "Beatriz", charLink: "https://www.rucoyonline.com/characters/Beatriz", pontos: 4 },
 ]

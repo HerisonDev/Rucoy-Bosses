b00dd91a879413c43478858994a2f3c6c2054933
Herison <rucoyconta72@gmail.com>
2024-10-05 18:28:41 +0000

    update

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,7 +1,5 @@
 let jogadores = [
     {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 2
-    },
-    
-    { nick: "Isabele trans", charLink: "https://www.rucoyonline.com/characters/Isabele%20trans", pontos: 1 },
+        
+    }
 ]

d97d6ce0ae36dea693a87ff233e4974d0b7236bf
Herison <rucoyconta72@gmail.com>
2024-10-06 19:45:48 +0000

    test

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ client = Together(api_key=os.environ.get('TOGETHER_API_KEY'))
 
 response = client.chat.completions.create(
     model="meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo",
-    messages=[ {"role": "user", "content": [{ "type": "text", "text": "Envolva entre asteristicos o conteudo da mensagem em roxo,mas a mensagem em roxo deve conter as palavras 'found' e 'The Rat King',e a mensagem em roxo tambem nao deve conter 2 pontos(:),qualquer outra mensagem em roxo que nao tiver essas caracteristicas devem ser ignoradas.Eu nao quero que voce passe nenhuma instrucao e nenhuma outra mensagem,apenas o conteudo da mensagem em roxo e nada mais,seja direto!" },{ "type": "image_url", "image_url": { "url": 
+    messages=[ {"role": "user", "content": [{ "type": "text", "text": "Ola,por gentileza,eu quero que voce analise a imagem e encontre a mensagem em roxo em que contem a 'found',e lembrando que essa mensagem nao contem caracteres especiais como ! por exemplo,caso voce encontre alguma coisa que se assemelhe a um caracterr especial,use uma letra parecida com ele,e por ultimo,por gentileza,apenas retorne o conteudo da menssgem em roxo e nada mais,nada como 'a mensagem em roxa é' ou nenhum tipo de instrucao,e outra regra é que oque vem antes da palavra 'found' nao pode conter numeros tambem,e tambem antes do 'found' nao deve haver dois pontos(:),caso contrario trata-se de uma mensagem de 'found' fake,que deve ser ignorada,apenas depois pode conter,apenas o conteudo dessa mensagem,grato." },{ "type": "image_url", "image_url": { "url": 
             "https://raw.githubusercontent.com/HerisonDev/Rucoy-Bosses/refs/heads/main/img/1.jpg" }}]} ])
 
 print(response.choices[0].message.content)

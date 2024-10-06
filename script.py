4ef0575ef4c427487a94183783e05d0bccb56a35
Herison <rucoyconta72@gmail.com>
2024-10-06 12:33:21 +0000

    update

--- script.py
+++ script.py
@@ -4,8 +4,8 @@ from together import Together
 client = Together(api_key=os.environ.get('TOGETHER_API_KEY'))
 
 response = client.chat.completions.create(
-    model="meta-llama/Llama-3.2-11B-Vision-Instruct-Turbo",
-    messages=[ {"role": "user", "content": [{ "type": "text", "text": "Pegue o texto em roxo no canto inferior esquerdo,e escreva apenas o texto que esta a esquerda dos 2 pontos da mensagem,apenas isso,tudo que estiver a direito dos : deve ser ignorado." },{ "type": "image_url", "image_url": { "url": 
-            "https://raw.githubusercontent.com/HerisonDev/Rucoy-Bosses/refs/heads/main/img/Screenshot_20241005-231949.jpg" }}]} ])
+    model="meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo",
+    messages=[ {"role": "user", "content": [{ "type": "text", "text": "No canto inferior esquerdo,existe uma mensagem em roxo,eu quero que voce me retorne como resposta apenas o conteudo dessa mensagem em roxo,as demais mensagens das cores verde,amarelo e branca devem ser ignoradas.A sua resposta deve ser apenas o conteudo da mensagem em branco que contem as palavras 'found' e 'Rat King'." },{ "type": "image_url", "image_url": { "url": 
+            "https://raw.githubusercontent.com/HerisonDev/Rucoy-Bosses/refs/heads/main/img/1.jpg" }}]} ])
 
 print(response.choices[0].message.content)

6f80956721c0f797d19ea20ae4d9fc0a02b9c59f
Herison <rucoyconta72@gmail.com>
2024-10-06 12:39:52 +0000

    update

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ client = Together(api_key=os.environ.get('TOGETHER_API_KEY'))
 
 response = client.chat.completions.create(
     model="meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo",
-    messages=[ {"role": "user", "content": [{ "type": "text", "text": "No canto inferior esquerdo,existe uma mensagem em roxo,eu quero que voce me retorne como resposta apenas o conteudo dessa mensagem em roxo,as demais mensagens das cores verde,amarelo e branca devem ser ignoradas.A sua resposta deve ser apenas o conteudo da mensagem em branco que contem as palavras 'found' e 'Rat King'." },{ "type": "image_url", "image_url": { "url": 
+    messages=[ {"role": "user", "content": [{ "type": "text", "text": "Envolva entre asteristicos o conteudo da mensagem em roxo,mas a mensagem em roxo deve conter as palavras 'found' e 'The Rat King',e a mensagem em roxo tambem nao deve conter 2 pontos(:),qualquer outra mensagem em roxo que nao tiver essas caracteristicas devem ser ignoradas.Eu nao quero que voce passe nenhuma instrucao e nenhuma outra mensagem,apenas o conteudo da mensagem em roxo e nada mais,seja direto!" },{ "type": "image_url", "image_url": { "url": 
             "https://raw.githubusercontent.com/HerisonDev/Rucoy-Bosses/refs/heads/main/img/1.jpg" }}]} ])
 
 print(response.choices[0].message.content)

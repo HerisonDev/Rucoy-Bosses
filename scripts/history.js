695e77da965f808ec680df6db84916608f1b92a8
Herison <rucoyconta72@gmail.com>
2024-10-06 00:43:48 +0000

    update

--- scripts/history.js
+++ scripts/history.js
@@ -1,24 +1,3 @@
 let found = [
-    {
-        nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", server: "Asia 3", date: "06 Oct 00:14:54"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 3", date: "06 Oct 00:11:57"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 3", date: "06 Oct 00:10:19"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 2", date: "06 Oct 00:08:56"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 1", date: "06 Oct 00:06:56"
-    },
-    {
-        nick: "Isadora Rola", charLink: "https://www.rucoyonline.com/characters/Isadora%20Rola", server: "North America 1", date: "06 Oct 00:05:25"
-    },
-    {
-        nick: "Isadora Rola", charLink: "https://www.rucoyonline.com/characters/Isadora%20Rola", server: "Asia 1", date: "06 Oct 00:00:08"
-    },
-    
+        
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,8 +1,3 @@
 let jogadores = [
-    {
-        nick: "Isadora Rola", charLink: "https://www.rucoyonline.com/characters/Isadora%20Rola", pontos: 2
-    }
     
-    ,{ nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 4 },
-    ,{ nick: "Yaguchi Ha", charLink: "https://www.rucoyonline.com/characters/Yaguchi%20Ha", pontos: 1 },
 ]

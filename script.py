30692f0b25746727d646398a6e2e24dcd4e53a6e
Herison <rucoyconta72@gmail.com>
2024-10-06 11:27:48 +0000

    update

--- script.py
+++ script.py
@@ -6,6 +6,6 @@ client = Together(api_key=os.environ.get('TOGETHER_API_KEY'))
 response = client.chat.completions.create(
     model="meta-llama/Llama-3.2-11B-Vision-Instruct-Turbo",
     messages=[ {"role": "user", "content": [{ "type": "text", "text": "Ola,voce pode descrever essa imagem??" },{ "type": "image_url", "image_url": { "url": 
-            "https://cursos.alura.com.br/assets/images/immersion/logo_imersao_dev_gemini.png" }}]} ])
+            "https://raw.githubusercontent.com/HerisonDev/Rucoy-Bosses/refs/heads/main/img/Screenshot_20241005-231949.jpg" }}]} ])
 
 print(response.choices[0].message.content)

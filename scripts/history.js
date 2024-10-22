518ff073ca5a854b28a8863184886b13937a067e
Herison <rucoyconta72@gmail.com>
2024-10-22 15:47:35 +0000

    clean

--- scripts/history.js
+++ scripts/history.js
@@ -1,15 +1,3 @@
 let found = [
-    {
-        nick: "Joao", charLink: "https://www.rucoyonline.com/characters/Joao", server: "Europe 1", date: "06 Oct 14:59:45"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 1", date: "06 Oct 14:56:44"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 3", date: "06 Oct 14:52:46"
-    },
-    {
-        nick: "Jose", charLink: "https://www.rucoyonline.com/characters/Jose", server: "Asia 4", date: "06 Oct 14:51:16"
-    },
-    
+
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,8 +1,3 @@
 let jogadores = [
-    {
-        nick: "Jose", charLink: "https://www.rucoyonline.com/characters/Jose", pontos: 1
-    }
-    
-    ,{ nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 2 },
-    ,{ nick: "Joao", charLink: "https://www.rucoyonline.com/characters/Joao", pontos: 1 },
+
 ]

31cc8e32e0905493de53db914f177f76c920cc1a
Herison <rucoyconta72@gmail.com>
2024-10-22 20:55:23 +0000

    clean

--- scripts/history.js
+++ scripts/history.js
@@ -1,9 +1,3 @@
 let found = [
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 3", date: "22 Oct 20:52:53"
-    },
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", server: "Asia 4", date: "22 Oct 20:52:04"
-    },
 
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,6 +1,3 @@
 let jogadores = [
-    {
-        nick: "Bestial", charLink: "https://www.rucoyonline.com/characters/Bestial", pontos: 2
-    }
 
 ]

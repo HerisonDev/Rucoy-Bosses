1b33a9f05927a3056684f931542a0cd65ccfcb27
Herison <rucoyconta72@gmail.com>
2024-10-23 16:49:22 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,3 +1,6 @@
 let found = [
+    {
+        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", server: "Asia 4", date: "23 Oct 16:49:22", boss: "Haunted Willow"
+    },
         
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,3 +1,6 @@
 let jogadores = [
+    {
+        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", pontos: 1
+    }
     
 ]

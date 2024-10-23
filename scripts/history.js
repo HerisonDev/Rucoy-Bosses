4d42b1cf82c8401dfa132dd65fc7c9e34e663ec8
Herison <rucoyconta72@gmail.com>
2024-10-23 16:43:40 +0000

    clean

--- scripts/history.js
+++ scripts/history.js
@@ -1,18 +1,3 @@
 let found = [
-    {
-        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", server: "Asia 1", date: "23 Oct 16:33:06", boss: "Haunted Willow"
-    },
-    {
-        nick: "Betial Titan", charLink: "https://www.rucoyonline.com/characters/Betial%20Titan", server: "Asia 2", date: "23 Oct 11:29:49", boss: "Haunted Willow"
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "Asia 2", date: "23 Oct 11:28:32", boss: "La Calaca"
-    },
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "Europe 1", date: "23 Oct 02:32:46", boss: "Haunted Willow"
-    },
-    {
-        nick: "Beatial Titan", charLink: "https://www.rucoyonline.com/characters/Beatial%20Titan", server: "Asia 4", date: "23 Oct 01:40:40", boss: "La Calaca"
-    },
-    
+        
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,10 +1,3 @@
 let jogadores = [
-    {
-        nick: "Beatial Titan", charLink: "https://www.rucoyonline.com/characters/Beatial%20Titan", pontos: 1
-    }
     
-    ,{ nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1 },
-    ,{ nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 1 },
-    ,{ nick: "Betial Titan", charLink: "https://www.rucoyonline.com/characters/Betial%20Titan", pontos: 1 },
-    ,{ nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", pontos: 1 },
 ]

dc04612c5f417a418cbf6ea5276ed0f66c4bc778
Herison <rucoyconta72@gmail.com>
2024-10-23 18:59:40 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Vsoeo", charLink: "https://www.rucoyonline.com/characters/Vsoeo", server: "Asia 1", date: "23 Oct 18:59:40", boss: "La Calaca"
+    },
+    {
         nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "Europe 3", date: "23 Oct 18:18:49", boss: "Haunted Willow"
     },
             
--- scripts/rank.js
+++ scripts/rank.js
@@ -3,4 +3,5 @@ let jogadores = [
         nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 1
     }
     
+    ,{ nick: "Vsoeo", charLink: "https://www.rucoyonline.com/characters/Vsoeo", pontos: 1 },
 ]

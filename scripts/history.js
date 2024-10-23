b5ab17b9da95b18d21ee5fb509030a31ab9a1e0d
Herison <rucoyconta72@gmail.com>
2024-10-23 02:32:46 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "Europe 1", date: "23 Oct 02:32:46", boss: "Haunted Willow"
+    },
+    {
         nick: "Beatial Titan", charLink: "https://www.rucoyonline.com/characters/Beatial%20Titan", server: "Asia 4", date: "23 Oct 01:40:40", boss: "La Calaca"
     },
     
--- scripts/rank.js
+++ scripts/rank.js
@@ -3,4 +3,5 @@ let jogadores = [
         nick: "Beatial Titan", charLink: "https://www.rucoyonline.com/characters/Beatial%20Titan", pontos: 1
     }
     
+    ,{ nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1 },
 ]

3986349faa3284a1e72b9be9d76054672308f4ba
Herison <rucoyconta72@gmail.com>
2024-10-23 00:01:27 +0000

    clean

--- scripts/history.js
+++ scripts/history.js
@@ -1,9 +1,3 @@
 let found = [
-    {
-        nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", server: "Asia 1", date: "22 Oct 23:39:30"
-    },
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", server: "Asia 4", date: "22 Oct 21:31:43"
-    },
-
+    
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,7 +1,3 @@
 let jogadores = [
-    {
-        nick: "Bestial Titan", charLink: "https://www.rucoyonline.com/characters/Bestial%20Titan", pontos: 1
-    }
-
-    ,{ nick: "Beatriz Cracuda", charLink: "https://www.rucoyonline.com/characters/Beatriz%20Cracuda", pontos: 1 },
+    
 ]

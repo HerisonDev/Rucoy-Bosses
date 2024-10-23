c07fcbc55929e3126281eea4bf421ca93f08f589
Herison <rucoyconta72@gmail.com>
2024-10-23 16:56:45 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Quexada", charLink: "https://www.rucoyonline.com/characters/Quexada", server: "Asia 3", date: "23 Oct 16:56:45", boss: "Haunted Willow"
+    },
+    {
         nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", server: "Asia 3", date: "23 Oct 16:54:44", boss: "La Calaca"
     },
     {
--- scripts/rank.js
+++ scripts/rank.js
@@ -3,4 +3,5 @@ let jogadores = [
         nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", pontos: 3
     }
     
+    ,{ nick: "Quexada", charLink: "https://www.rucoyonline.com/characters/Quexada", pontos: 1 },
 ]

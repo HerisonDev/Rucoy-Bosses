7e0b0cad1b692be910f4cea26d809f9e57acffc3
Herison <rucoyconta72@gmail.com>
2024-10-23 18:09:34 +0000

    clean

--- scripts/history.js
+++ scripts/history.js
@@ -1,33 +1,3 @@
 let found = [
-    {
-        nick: "Nforie", charLink: "https://www.rucoyonline.com/characters/Nforie", server: "Asia 1", date: "23 Oct 18:02:40", boss: "La Calaca"
-    },
-    {
-        nick: "Jriroroe", charLink: "https://www.rucoyonline.com/characters/Jriroroe", server: "Asia 1", date: "23 Oct 18:01:42", boss: "Haunted Willow"
-    },
-    {
-        nick: "Haaaa", charLink: "https://www.rucoyonline.com/characters/Haaaa", server: "Asia 2", date: "23 Oct 17:58:24", boss: "Haunted Willow"
-    },
-    {
-        nick: "Haaaa", charLink: "https://www.rucoyonline.com/characters/Haaaa", server: "Asia 2", date: "23 Oct 17:56:52", boss: "La Calaca"
-    },
-    {
-        nick: "Agr Desbugou", charLink: "https://www.rucoyonline.com/characters/Agr%20Desbugou", server: "Asia 4", date: "23 Oct 17:09:41", boss: "La Calaca"
-    },
-    {
-        nick: "Desbugoo", charLink: "https://www.rucoyonline.com/characters/Desbugoo", server: "Asia 4", date: "23 Oct 17:03:57", boss: "La Calaca"
-    },
-    {
-        nick: "Quexada", charLink: "https://www.rucoyonline.com/characters/Quexada", server: "Asia 3", date: "23 Oct 16:56:45", boss: "Haunted Willow"
-    },
-    {
-        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", server: "Asia 3", date: "23 Oct 16:54:44", boss: "La Calaca"
-    },
-    {
-        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", server: "Asia 4", date: "23 Oct 16:53:16", boss: "La Calaca/Haunted Willow"
-    },
-    {
-        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", server: "Asia 4", date: "23 Oct 16:49:22", boss: "Haunted Willow"
-    },
-        
+            
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,12 +1,3 @@
 let jogadores = [
-    {
-        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", pontos: 3
-    }
     
-    ,{ nick: "Quexada", charLink: "https://www.rucoyonline.com/characters/Quexada", pontos: 1 },
-    ,{ nick: "Desbugoo", charLink: "https://www.rucoyonline.com/characters/Desbugoo", pontos: 1 },
-    ,{ nick: "Agr Desbugou", charLink: "https://www.rucoyonline.com/characters/Agr%20Desbugou", pontos: 1 },
-    ,{ nick: "Haaaa", charLink: "https://www.rucoyonline.com/characters/Haaaa", pontos: 2 },
-    ,{ nick: "Jriroroe", charLink: "https://www.rucoyonline.com/characters/Jriroroe", pontos: 1 },
-    ,{ nick: "Nforie", charLink: "https://www.rucoyonline.com/characters/Nforie", pontos: 1 },
 ]

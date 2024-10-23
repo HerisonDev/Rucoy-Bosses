9b01d72792477c3800fd3b4c4c42c01501a882f2
Herison <rucoyconta72@gmail.com>
2024-10-23 01:40:40 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,3 +1,6 @@
 let found = [
+    {
+        nick: "Beatial Titan", charLink: "https://www.rucoyonline.com/characters/Beatial%20Titan", server: "Asia 4", date: "23 Oct 01:40:40"
+    },
     
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,3 +1,6 @@
 let jogadores = [
+    {
+        nick: "Beatial Titan", charLink: "https://www.rucoyonline.com/characters/Beatial%20Titan", pontos: 1
+    }
     
 ]

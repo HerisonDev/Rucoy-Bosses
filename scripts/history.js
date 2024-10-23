1b37667c9b0b32a5b259c593ad86cf8159e4ef14
Herison <rucoyconta72@gmail.com>
2024-10-23 16:54:44 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", server: "Asia 3", date: "23 Oct 16:54:44", boss: "La Calaca"
+    },
+    {
         nick: "Il Hail Ykz", charLink: "https://www.rucoyonline.com/characters/Il%20Hail%20Ykz", server: "Asia 4", date: "23 Oct 16:53:16", boss: "La Calaca/Haunted Willow"
     },
     {

827c4deb57f3ca3bc4cf0eeef0c2c8626dc554b4
Herison <rucoyconta72@gmail.com>
2024-10-24 14:10:35 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "Kjrore", charLink: "https://www.rucoyonline.com/characters/Kjrore", server: "North America 1", date: "24 Oct 14:10:35", boss: "La Calaca"
+    },
+    {
         nick: "Kfgtre", charLink: "https://www.rucoyonline.com/characters/Kfgtre", server: "North America 1", date: "24 Oct 14:08:26", boss: "Haunted Willow"
     },
                 
--- scripts/rank.js
+++ scripts/rank.js
@@ -3,4 +3,5 @@ let jogadores = [
         nick: "Kfgtre", charLink: "https://www.rucoyonline.com/characters/Kfgtre", pontos: 1
     }
     
+    ,{ nick: "Kjrore", charLink: "https://www.rucoyonline.com/characters/Kjrore", pontos: 1 },
 ]

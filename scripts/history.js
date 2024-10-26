96d4e6667388ec86df1e18e74d71dfbb42be319d
Herison <rucoyconta72@gmail.com>
2024-10-26 20:40:39 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "System", charLink: "https://www.rucoyonline.com/characters/System", server: "North America 6", date: "26 Oct 20:40:38", boss: "Haunted Willow"
+    },
+    {
         nick: "System", charLink: "https://www.rucoyonline.com/characters/System", server: "North America 1", date: "26 Oct 20:33:54", boss: "Haunted Willow"
     },
     

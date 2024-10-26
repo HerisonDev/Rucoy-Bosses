ec908ed5f22e1216b3c9f2451fc545a10c3f3c76
Herison <rucoyconta72@gmail.com>
2024-10-26 00:07:55 +0000

    create logic

---
+++ scripts/historyPage.js
@@ -0,0 +1,12 @@
+const historyBoard = document.getElementById('sec-history');
+
+limitedFound = found.slice(0, 5);
+
+for (let history of limitedFound) {
+    historyContent +=
+        `
+          <p><span class="time-span">${history.date} :</span> <a href="${history.charLink}" target="_blank">${history.nick}</a> found <span class="boss-name">${history.boss}</span> on ${history.server}.</p>
+            `
+}
+
+historyBoard.innerHTML += historyContent;

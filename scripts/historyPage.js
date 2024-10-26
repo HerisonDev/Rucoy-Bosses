d12f515e9b02d6d97ebfe151886eeb3bba027c61
Herison <rucoyconta72@gmail.com>
2024-10-26 01:03:57 +0000

    implement historyPage

--- scripts/historyPage.js
+++ scripts/historyPage.js
@@ -1,16 +1,18 @@
 let historyContent = "";
 let limitedFound = "";
 
+const sectionHistory = document.getElementById('sec-history');
+
 const historyBoard = document.getElementById('sec-history');
 
-limitedFound = found.slice(0, 5);
+//limitedFound = found.slice(0, 10);
 
-for(let history of limitedFound){
+for(let history of found){
   historyContent +=
   `
   <p><span class="time-span">${history.date} :</span> <a href="${history.charLink}" target="_blank">${history.nick}</a> found <span class="boss-name">${history.boss}</span> on ${history.server}.</p>
   `
 
-
-historyBoard.innerHTML += historyContent;
 }
+historyBoard.innerHTML += historyContent;
+

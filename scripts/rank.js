f1414e0b29a4a722c80daf63cd202435243befc2
Herison <rucoyconta72@gmail.com>
2024-10-26 19:36:07 +0000

    clean

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,18 +1,3 @@
 let jogadores = [
-    {
-        nick: "Kfgtre", charLink: "https://www.rucoyonline.com/characters/Kfgtre", pontos: 1
-    }
     
-    ,{ nick: "Kjrore", charLink: "https://www.rucoyonline.com/characters/Kjrore", pontos: 1 },
-    ,{ nick: "Mlk Inutil", charLink: "https://www.rucoyonline.com/characters/Mlk%20Inutil", pontos: 1 },
-    ,{ nick: "Hjj", charLink: "https://www.rucoyonline.com/characters/Hjj", pontos: 1 },
-    ,{ nick: "Ngori", charLink: "https://www.rucoyonline.com/characters/Ngori", pontos: 1 },
-    ,{ nick: "Jose", charLink: "https://www.rucoyonline.com/characters/Jose", pontos: 1 },
-    ,{ nick: "Jtjr", charLink: "https://www.rucoyonline.com/characters/Jtjr", pontos: 1 },
-    ,{ nick: "Jfjfi", charLink: "https://www.rucoyonline.com/characters/Jfjfi", pontos: 1 },
-    ,{ nick: "Wjwjw", charLink: "https://www.rucoyonline.com/characters/Wjwjw", pontos: 1 },
-    ,{ nick: "Ekeie", charLink: "https://www.rucoyonline.com/characters/Ekeie", pontos: 1 },
-    ,{ nick: "Otario", charLink: "https://www.rucoyonline.com/characters/Otario", pontos: 1 },
-    ,{ nick: "Fgrgem", charLink: "https://www.rucoyonline.com/characters/Fgrgem", pontos: 1 },
-    ,{ nick: "Besjk", charLink: "https://www.rucoyonline.com/characters/Besjk", pontos: 1 },
 ]

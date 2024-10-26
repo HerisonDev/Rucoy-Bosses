bd2e8b0b43b00900e0c33e565e661e4d78008a3e
Herison <rucoyconta72@gmail.com>
2024-10-26 01:41:16 +0000

    fix float menu

--- scripts/historyPage.js
+++ scripts/historyPage.js
@@ -1,15 +1,23 @@
 let historyContent = "";
 let limitedFound = "";
+const menuButton = document.getElementById('menu-button');
+const floatMenu = document.getElementById('float-menu');
 
 const sectionHistory = document.getElementById('sec-history');
 
 const historyBoard = document.getElementById('sec-history');
 
+menuButton.addEventListener('click', () => {
+    // Toggle a classe 'hidden' para mostrar ou ocultar o menu
+    floatMenu.classList.toggle('hidden');
+});
+
+
 //limitedFound = found.slice(0, 10);
 
-for(let history of found){
-  historyContent +=
-  `
+for (let history of found) {
+    historyContent +=
+        `
   <p><span class="time-span">${history.date} :</span> <a href="${history.charLink}" target="_blank">${history.nick}</a> found <span class="boss-name">${history.boss}</span> on ${history.server}.</p>
   `
 

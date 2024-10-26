6a32f5419a14dddcc2afe8bc7d1a099c3ff4759a
Herison <rucoyconta72@gmail.com>
2024-10-26 00:48:26 +0000

    initial support for history page

--- scripts/historyPage.js
+++ scripts/historyPage.js
@@ -1,12 +1,16 @@
+let historyContent = "";
+let limitedFound = "";
+
 const historyBoard = document.getElementById('sec-history');
 
 limitedFound = found.slice(0, 5);
 
-for (let history of limitedFound) {
-    historyContent +=
-        `
-          <p><span class="time-span">${history.date} :</span> <a href="${history.charLink}" target="_blank">${history.nick}</a> found <span class="boss-name">${history.boss}</span> on ${history.server}.</p>
-            `
-}
+for(let history of limitedFound){
+  historyContent +=
+  `
+  <p><span class="time-span">${history.date} :</span> <a href="${history.charLink}" target="_blank">${history.nick}</a> found <span class="boss-name">${history.boss}</span> on ${history.server}.</p>
+  `
+
 
 historyBoard.innerHTML += historyContent;
+}

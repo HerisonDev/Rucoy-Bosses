178b5c96fff64ee62f3455c3d95b00b50a211479
Herison <rucoyconta72@gmail.com>
2024-10-28 01:00:01 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,8 @@
 let found = [
     {
+        nick: "System", charLink: "https://www.rucoyonline.com/characters/System", server: "Europe 1", date: "28 Oct 01:00:01", boss: "La Calaca"
+    },
+    {
         nick: "System", charLink: "https://www.rucoyonline.com/characters/System", server: "North America 6", date: "26 Oct 20:40:38", boss: "Haunted Willow"
     },
     {
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,6 +1,6 @@
 let jogadores = [
     {
-        nick: "System", charLink: "https://www.rucoyonline.com/characters/System", pontos: 2
+        nick: "System", charLink: "https://www.rucoyonline.com/characters/System", pontos: 3
     }
     
 ]

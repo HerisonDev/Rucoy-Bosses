87e5d2ead6b8b282eaa4bf892dce714f6321de5f
Herison <rucoyconta72@gmail.com>
2024-11-02 14:14:10 +0000

    Add timezone

--- scripts/app.js
+++ scripts/app.js
@@ -82,7 +82,7 @@ limitedFound = found.slice(0, 5);
 for(let history of limitedFound){
   historyContent +=
   `
-  <p><span class="time-span">${history.date} :</span> <a href="${history.charLink}" target="_blank">${history.nick}</a> found <span class="boss-name">${history.boss}</span> on ${history.server}.</p>
+  <p><span class="time-span">UTC-3 ${history.date} :</span> <a href="${history.charLink}" target="_blank">${history.nick}</a> found <span class="boss-name">${history.boss}</span> on ${history.server}.</p>
   `
 }
 

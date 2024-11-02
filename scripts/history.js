2242c9e51bb03001f077afbbee98d797a65ca267
Herison <rucoyconta72@gmail.com>
2024-11-02 14:07:52 +0000

    update rank.js and history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,3 +1,6 @@
 let found = [
+    {
+        nick: "Teste", charLink: "https://www.rucoyonline.com/characters/Teste", server: "Asia 1", date: "02 Nov 11:07:52", boss: "Haunted Willow"
+    },
         
 ];
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,3 +1,6 @@
 let jogadores = [
+    {
+        nick: "Teste", charLink: "https://www.rucoyonline.com/characters/Teste", pontos: 1
+    }
         
 ]

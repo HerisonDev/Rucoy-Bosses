f6dd65073bd3612c4d210600284f14678b204c12
Herison <rucoyconta72@gmail.com>
2024-11-02 20:00:01 +0000

    typo

--- script.py
+++ script.py
@@ -17,7 +17,7 @@ response = client.chat.completions.create(
     messages=[{
         "role": "user",
         "content": [
-            {"type": "text", "text": "Analise a imagem e me retorne apenas a mensagem em roxo,que tem uma fonte baseada em 8-bits,que fica no canto inferior,porem eu quero que voce pegue apenas a mensagem que nao possui caracteres especiais como '!',':' e numeros,,e se tiver mais de uma mensagem roxa,analise bem as mensagens e retorne apenas a mensagem que contem a dewcricao passada,e eu quero que voce retorne apenas uma mensagem roxa,pois se voce analisar na inagem,cada mensagem fica embaixo da outra,entao retorne apenas uma,e por ultimo,eu quero que voce seja direto na resposta,apenas retornando a mensagem roxa.E se voce analisar a imagem e nao encontrar nenhuma mensagem roxa que tenha essas caracteristicas,retorne a mensagem 'not found'."},
+            {"type": "text", "text": "Analise a imagem e me retorne apenas a mensagem em roxo,que tem uma fonte baseada em 8-bits,porem eu quero que voce pegue apenas a mensagem que nao possui caracteres especiais como '!',':' e numeros,e se tiver mais de uma mensagem roxa,analise bem as mensagens e retorne apenas a mensagem que contem a descricao passada,e eu quero que voce retorne apenas uma mensagem roxa,pois se voce analisar na imagem,cada mensagem fica embaixo da outra,entao retorne apenas uma,e por ultimo,eu quero que voce seja direto na resposta,apenas retornando a mensagem roxa.E se voce analisar a imagem e nao encontrar nenhuma mensagem roxa que tenha essas caracteristicas,retorne a mensagem 'not found'."},
             {"type": "image_url", "image_url": {"url": url_da_imagem}}
         ]
     }]
